853f3fb17e74c85ca5506ca7763774bb44be33da
Tiffany Townsend <172116646+TZTownsend@users.noreply.github.com>
2025-07-29 19:30:18 +0800

    refactor: Move final verse from for block

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -3,22 +3,24 @@ NUMBER_OF_BOTTLES = 99
 
 def main():
     bottle_or_bottles = "bottles"
-    drink_or_store = "Take one down, and pass it around,"
 
     for bottles in range(NUMBER_OF_BOTTLES, 0, -1):
         print(f"{bottles} {bottle_or_bottles} of beer on the wall,\n"
-              f"{bottles} {bottle_or_bottles} of beer,\n"
-              f"{drink_or_store}")
+              f"{bottles} {bottle_or_bottles} of beer,")
         bottles -= 1
+        if bottles == 0:
+            bottles = "No more"
         if bottles == 1:
             bottle_or_bottles = "bottle"
         else:
             bottle_or_bottles = "bottles"
-        if bottles == 0:
-            drink_or_store = "Go to the store and buy some more,"
-        else:
-            drink_or_store = "Take one down, and pass it around,"
-        print(f"{bottles} {bottle_or_bottles} of beer on the wall!\n")
+        print("Take one down, and pass it around,\n"
+              f"{bottles} {bottle_or_bottles} of beer on the wall!\n")
+    else:
+        print("No more bottles of beer on the wall,\n"
+              "No more bottles of beer,\n"
+              "Go to the store and buy some more,\n"
+              f"{NUMBER_OF_BOTTLES} bottles of beer on the wall.\n")
 
 
 if __name__ == "__main__":
